78db570ccdc6bc051e950aacc1e42e20fa6e7c02
suminLee <tnalswkd1122@naver.com>
2023-05-03 18:08:00 +0900

    데이터 동적 렌더링, map 함수 이용

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,7 +6,7 @@ import Card from '../UI/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
-  const [filteredYar, setFilteredYear] = useState('2023');
+  const [filteredYear, setFilteredYear] = useState('2023');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
@@ -16,29 +16,16 @@ const Expenses = (props) => {
     <div>
       <Card className='expenses'>
         <ExpenseFilter
-          selected={filteredYar}
+          selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
